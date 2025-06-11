d6bbe32df6264759e1ece80695a313a2fbb0c1a5
Yohen Sheraun <yosherau@u91z04s01.42kl.edu.my>
2025-06-11 14:08:43 +0800

    Changed order of meals_eaten updated

--- srcs/start_eating.c
+++ srcs/start_eating.c
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:03:13 by yosherau          #+#    #+#             */
-/*   Updated: 2025/06/10 19:47:49 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/06/11 14:07:54 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	start_eating(t_philo *philo, t_prog *prog)
 	print_status(philo, prog, YELLOW "has taken a fork" RST);
 	set_long(&philo->last_meal_lock, &philo->last_meal, get_time(MILLI));
 	print_status(philo, prog, GREEN "is eating" RST);
-	set_long(&philo->meal_lock, &philo->meals_eaten, philo->meals_eaten + 1);
 	precise_sleep(prog->time_to_eat * 1000, prog);
+	set_long(&philo->meal_lock, &philo->meals_eaten, philo->meals_eaten + 1);
 	pthread_mutex_unlock(&philo->first_fork->fork);
 	pthread_mutex_unlock(&philo->second_fork->fork);
 }
